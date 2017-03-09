c1dadcdd66b2b591771dddc7a713cf4c2c9107f5
Ernesto Celis <developer@celisdelafuente.net>
2017-03-09 16:58:20 -0600

    player working

--- monroe/monroe.py
+++ monroe/monroe.py
@@ -24,11 +24,16 @@ def main():
             flag = read()
             if flag == "/":
                 exit(True)
+            if flag == "1":
+                play("./audio/01.mp3")
+            if flag == "2":
+                play("./audio/02.mp3")
 
             logging.info(flag)
 
         except Exception:
-            sys.exit(0)
+            logging.error("Exception")
+            sys.exit(-1)
 
 
 def exit(flag):
@@ -44,8 +49,11 @@ def log_init():
 
 
 def play(audio):
-    p = vlc.MediaPlayer(audio)
-    p.play()
+    logging.info(audio)
+    player = vlc.MediaPlayer(audio)
+    player.play()
+
+
 
 if __name__ == "__main__":
     main()
